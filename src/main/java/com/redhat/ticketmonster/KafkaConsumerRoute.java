7fba922a4aa6b40bfde6f69fb6062d95fe4bfa58
Christian Posta <christian.posta@gmail.com>
2016-06-20 21:51:33 -0700

    inital commit

---
+++ src/main/java/com/redhat/ticketmonster/KafkaConsumerRoute.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.redhat.ticketmonster;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ceposta 
+ * <a href="http://christianposta.com/blog>http://christianposta.com/blog</a>.
+ */
+@Component
+public class KafkaConsumerRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("kafka:kafka:9092?topic=mysql-server-1.ticketmonster.Booking&groupId=foo-group&autoOffsetReset=earliest" +
+                "&consumersCount=1").process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                System.out.println("found a new message");
+            }
+        });
+    }
+}
